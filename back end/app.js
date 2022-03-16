0f0f41d328239efc225deb7b9c2f2d997f9d1760
AlexandreFontainee <71234514+AlexandreFontainee@users.noreply.github.com>
2022-03-16 07:40:57 +0100

    commit

--- back end/app.js
+++ back end/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+
 
 // routes
 const userRoutes = require('./routes/userRoutes');
@@ -15,15 +17,17 @@ mongoose.connect('mongodb+srv://alexandre:NewPassword44@cluster0.k8pza.mongodb.n
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
-    next()
-})
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+//     next()
+// })
 
-app.use(bodyParser.json());
+
+// app.use(bodyParser.json());
 app.use(express.json());
+app.use(cors({origin: "http://127.0.0.1:8081"}));
 
 // routes 
 
